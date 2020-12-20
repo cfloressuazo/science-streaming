19f5fde84f7a9a12e76e27c2aa1c78ac37e2d0ef
Cesar Flores <cfloressuazo@gmail.com>
2020-12-20 14:06:57 -0300

    working faust simple consumer

--- consumers/faust_project/medicare/agents.py
+++ consumers/faust_project/medicare/agents.py
@@ -4,7 +4,7 @@ from faust_project.app import app
 from faust_project.codecs.avro import avro_medicare_value_serializer, avro_medicare_key_serializer
 from faust_project.medicare.models import MedicareValueModel, MedicareKeyModel
 
-medicare_topic = app.topic('medicare', partitions=1, value_type=MedicareValueModel)
+medicare_topic = app.topic('test', partitions=1, value_type=MedicareValueModel)
 
 logger = logging.getLogger(__name__)
 
@@ -12,5 +12,6 @@ logger = logging.getLogger(__name__)
 @app.agent(medicare_topic)
 async def medicare(medicare):
     async for record in medicare:
-        logger.info("Event received in topic org.science.medicare")
+        # logger.info(record)
         logger.info(f"Provider type: {record.provider_type}")
+        logger.info(f"Average medicare spending: {record.average_medicare_payment_amt}")
